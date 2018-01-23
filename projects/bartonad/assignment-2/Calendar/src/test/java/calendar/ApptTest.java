81205c83adca8bc6b5b3357a5064581dd0eb0f04
Adam Barton <adamb6102@gmail.com>
2018-01-22 21:34:47 -0800

    Added test files

---
+++ projects/bartonad/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -0,0 +1,300 @@
+package calendar;
+/**
+ *  This class provides a basic set of test cases for the
+ *  Appt class.
+ */
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class ApptTest {
+    /**
+     * Test that the gets methods work as expected.
+     */
+	 @Test
+	  public void testConstructor()  throws Throwable  {
+		 int startHour=21;
+		 int startMinute=30;
+		 int startDay=15;
+		 int startMonth=01;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+	// assertions
+		 assertTrue(appt.getValid());
+		 assertEquals(21, appt.getStartHour());
+		 assertEquals(30, appt.getStartMinute());
+		 assertEquals(15, appt.getStartDay());
+		 assertEquals(01, appt.getStartMonth());
+		 assertEquals(2018, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());         		
+	 }
+
+	 @Test
+	  public void testInvalid()  throws Throwable  {
+		 int startHour=28;
+		 int startMinute=30;
+		 int startDay=15;
+		 int startMonth=01;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data
+		 Appt appt = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 title,
+				 description);
+
+		 startHour = -1;
+		 Appt appt2 = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 title,
+				 description);
+		 // assertions
+		 assertFalse(appt.getValid());
+		 assertFalse(appt2.getValid());
+	 }
+	@Test
+	public void testInvalid2()  throws Throwable  {
+		int startHour=21;
+		int startMinute=65;
+		int startDay=15;
+		int startMonth=01;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+
+		startMinute = -1;
+		Appt appt2 = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		// assertions
+		assertFalse(appt.getValid());
+		assertFalse(appt2.getValid());
+	}
+	@Test
+	public void testInvalid3()  throws Throwable  {
+		int startHour=21;
+		int startMinute=50;
+		int startDay=36;
+		int startMonth=01;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+
+		startDay=-1;
+		Appt appt2 = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		// assertions
+		assertFalse(appt.getValid());
+		assertFalse(appt2.getValid());
+	}
+	@Test
+	public void testInvalidBranches()  throws Throwable  {
+		int startHour=21;
+		int startMinute=50;
+		int startDay=36;
+		int startMonth=01;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+
+
+		title=null;
+		description=null;
+		appt.setTitle(title);
+		appt.setDescription(description);
+		// assertions
+		assertFalse(appt.getValid());
+	}
+
+	@Test
+	public void testSetFunctions()  throws Throwable  {
+		int startHour=21;
+		int startMinute=30;
+		int startDay=15;
+		int startMonth=01;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		int newStartHour=12;
+		int newStartMinute=45;
+		int newStartDay=10;
+		int newStartMonth=02;
+		int newStartYear=2019;
+		String newTitle="Friend Party";
+		String newDescription="This is a party for my friends.";
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		appt.setStartHour(newStartHour);
+		appt.setStartMinute(newStartMinute);
+		appt.setStartDay(newStartDay);
+		appt.setStartMonth(newStartMonth);
+		appt.setStartYear(newStartYear);
+		appt.setTitle(newTitle);
+		appt.setDescription(newDescription);
+		// assertions
+		assertTrue(appt.getValid());
+		assertEquals(12, appt.getStartHour());
+		assertEquals(45, appt.getStartMinute());
+		assertEquals(10, appt.getStartDay());
+		assertEquals(02, appt.getStartMonth());
+		assertEquals(2019, appt.getStartYear());
+		assertEquals("Friend Party", appt.getTitle());
+		assertEquals("This is a party for my friends.", appt.getDescription());
+	}
+
+	@Test
+	public void testToString()  throws Throwable  {
+		int startHour=21;
+		int startMinute=50;
+		int startDay=15;
+		int startMonth=01;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+
+		startDay = 200;
+		Appt appt2 = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		startDay = 15;
+		startHour = 10;
+		Appt appt3 = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		// assertions
+		assertNull(appt2.toString());
+		assertEquals("	1/15/2018 at 9:50pm ,Birthday Party, This is my birthday party.\n", appt.toString());
+		assertEquals("	1/15/2018 at 10:50am ,Birthday Party, This is my birthday party.\n", appt3.toString());
+	}
+
+	@Test
+	public void testCompareTo()  throws Throwable  {
+		int startHour=12;
+		int startMinute=30;
+		int startDay=10;
+		int startMonth=03;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+
+		startHour=10;
+		startMinute=20;
+		startDay=05;
+		startMonth=02;
+		startYear=2018;
+		title="Birthday Party";
+		description="This is my birthday party.";
+		Appt appt2 = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		// assertions
+		assertEquals(18, appt.compareTo(appt2));
+	}
+
+	@Test
+	public void testIsRecurring()  throws Throwable  {
+		int startHour=12;
+		int startMinute=30;
+		int startDay=10;
+		int startMonth=03;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		int[] recurDaysArr={2,3,4};
+		appt.setRecurrence(recurDaysArr, Appt.RECUR_BY_WEEKLY, 2, Appt.RECUR_NUMBER_NEVER);
+		// assertions
+		assertFalse(appt.isRecurring());
+	}
+	
+}
---
+++ projects/bartonad/assignment-2/Calendar/src/test/java/calendar/TimeTableTest.java
@@ -0,0 +1,276 @@
+package calendar;
+/**
+ *  This class provides a basic set of test cases for the
+ *  TimeTable class.
+ */
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TimeTableTest {
+
+	 @Test
+	  public void testGetApptRange()  throws Throwable  {
+		 Calendar rightnow = Calendar.getInstance();
+		 int thisMonth = rightnow.get(Calendar.MONTH+1);
+		 int thisYear = rightnow.get(Calendar.YEAR);
+		 int thisDay = rightnow.get(Calendar.DAY_OF_MONTH);
+		 LinkedList<Appt> listAppts = new LinkedList<Appt>();
+
+		 GregorianCalendar today = new GregorianCalendar(thisYear,thisMonth,thisDay);
+		 GregorianCalendar tomorrow = (GregorianCalendar)today.clone();
+		 tomorrow.add(Calendar.DAY_OF_MONTH,1);
+
+
+		 int startHour=15;
+		 int startMinute=30;
+		 int startDay=thisDay+1;
+		 int startMonth=thisMonth;
+		 int startYear=thisYear;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data
+		 Appt appt = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 title,
+				 description);
+
+		 startHour=14;
+		 startMinute=30;
+		 startDay=thisDay;
+		 startMonth=thisMonth;
+		 startYear=thisYear;
+		 title="Class";
+		 description="Rescheduled class.";
+		 //Construct a new Appointment object with the initial data
+		 Appt appt2 = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 title,
+				 description);
+
+		 startHour=14;
+		 startMinute=30;
+		 startDay=thisDay+5;
+		 startMonth=thisMonth;
+		 startYear=thisYear;
+		 title="Class2";
+		 description="Gym class";
+		 //Construct a new Appointment object with the initial data
+		 Appt appt3 = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 title,
+				 description);
+
+		 listAppts.add(appt);
+		 listAppts.add(appt2);
+		 listAppts.add(appt3);
+
+		 TimeTable t = new TimeTable();
+		 assertEquals(1, t.getApptRange(listAppts, today, tomorrow).size());
+	 }
+
+	 @Test(expected = DateOutOfRangeException.class)
+	  public void testGetApptRangeError()  throws Throwable  {
+		 LinkedList<Appt> listAppts = new LinkedList<Appt>();
+
+		 Calendar rightnow = Calendar.getInstance();
+		 int thisMonth = rightnow.get(Calendar.MONTH)+1;
+		 int thisYear = rightnow.get(Calendar.YEAR);
+		 int thisDay = rightnow.get(Calendar.DAY_OF_MONTH);
+		 GregorianCalendar today = new GregorianCalendar(thisYear,thisMonth,thisDay);
+		 GregorianCalendar tomorrow = (GregorianCalendar)today.clone();
+		 today.add(Calendar.DAY_OF_MONTH,1);
+
+		 TimeTable t = new TimeTable();
+		 t.getApptRange(listAppts, today, tomorrow);
+
+	 }
+
+	@Test
+	public void testGetNextApptOccurrence()  throws Throwable  {
+
+		Calendar rightnow = Calendar.getInstance();
+		int thisMonth = rightnow.get(Calendar.MONTH)+1;
+		int thisYear = rightnow.get(Calendar.YEAR);
+		int thisDay = rightnow.get(Calendar.DAY_OF_MONTH);
+		int startHour=15;
+		int startMinute=30;
+		int startDay=thisDay+1;
+		int startMonth=thisMonth;
+		int startYear=thisYear;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+
+		int[] recurDaysArr={2,3,4};
+		appt.setRecurrence(recurDaysArr, Appt.RECUR_BY_WEEKLY, 2, Appt.RECUR_NUMBER_FOREVER);
+
+		GregorianCalendar today = new GregorianCalendar(thisYear,thisMonth,thisDay);
+		GregorianCalendar tomorrow = (GregorianCalendar)today.clone();
+		tomorrow.add(Calendar.MONTH,1);
+
+		LinkedList<Appt> listAppts = new LinkedList<Appt>();
+		listAppts.add(appt);
+
+		TimeTable t = new TimeTable();
+		GregorianCalendar day = new GregorianCalendar();
+		assertEquals(28, t.getApptRange(listAppts, today, tomorrow).size());
+
+	}
+
+	@Test
+	public void testDeleteAppt()  throws Throwable  {
+
+		Calendar rightnow = Calendar.getInstance();
+		int thisMonth = rightnow.get(Calendar.MONTH)+1;
+		int thisYear = rightnow.get(Calendar.YEAR);
+		int thisDay = rightnow.get(Calendar.DAY_OF_MONTH);
+		int startHour=15;
+		int startMinute=30;
+		int startDay=thisDay+1;
+		int startMonth=thisMonth;
+		int startYear=thisYear;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+
+		Appt appt2 = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+
+		Appt appt3 = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+
+		GregorianCalendar today = new GregorianCalendar(thisYear,thisMonth,thisDay);
+		GregorianCalendar tomorrow = (GregorianCalendar)today.clone();
+		tomorrow.add(Calendar.MONTH,1);
+
+		LinkedList<Appt> listAppts = new LinkedList<Appt>();
+		listAppts.add(appt);
+		listAppts.add(appt2);
+		listAppts.add(appt3);
+
+		TimeTable t = new TimeTable();
+
+		assertEquals(3, listAppts.size());
+		LinkedList<Appt> listDeletedAppts=t.deleteAppt(listAppts, listAppts.get(1));
+
+		assertEquals(2, listDeletedAppts.size());
+
+	}
+
+	@Test
+	public void testDeleteApptNull()  throws Throwable  {
+
+		LinkedList<Appt> listAppts = null;
+		Appt appt = null;
+		TimeTable t = new TimeTable();
+
+		assertNull(t.deleteAppt(listAppts, appt));
+
+		int startHour=21;
+		int startMinute=50;
+		int startDay=200;
+		int startMonth=01;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		listAppts = new LinkedList<Appt>();
+		listAppts.add(appt);
+		assertNull(t.deleteAppt(listAppts, appt));
+
+	}
+
+	@Test
+	public void testPermute()  throws Throwable  {
+
+		LinkedList<Appt> listAppts = new LinkedList<Appt>();
+		TimeTable t = new TimeTable();
+
+		int startHour=21;
+		int startMinute=50;
+		int startDay=15;
+		int startMonth=01;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		description="This is my friend's birthday party.";
+		Appt appt2 = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		description="This is my mom's birthday party.";
+		Appt appt3 = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		listAppts.add(appt);
+		listAppts.add(appt2);
+		listAppts.add(appt3);
+		int[] pv = new int[3];
+		pv[1] = 2;
+		LinkedList<Appt> permutedList = t.permute(listAppts, pv);
+//		System.out.println(listAppts);
+//		System.out.println(permutedList);
+		assertEquals(listAppts.get(0), permutedList.get(0));
+
+	}
+}
